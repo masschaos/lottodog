a77705b6b443ab76fb07f34538968d65bd4f7fe2
Jack Zhang <jhzzzz@gmail.com>
2020-08-09 09:22:03 +0800

    添加定时任务

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const CronJob = require('cron').CronJob
+const validateLotteryJob = require('./jobs/validate-lottery')
+const validateResultJob = require('./jobs/validate-result')
+const isDebug = process.env.DEBUG_MODE
+
+const cronjobLottery = new CronJob(
+  isDebug ? '*/3 * * * *' : '0 */1 * * *', // 每个整点检查一次
+  function () {
+    validateLotteryJob.executeJob()
+  }
+)
+
+const cronjobResult = new CronJob(
+  isDebug ? '*/3 * * * *' : '*/30 * * * *', // 每半小时检查一次,
+  function () {
+    validateResultJob.executeJob()
+  }
+)
+
+// now start jobs
+cronjobLottery.start()
+cronjobResult.start()
--- src/jobs/validate-result.js
+++ src/jobs/validate-result.js
@@ -15,52 +15,54 @@ const im = new IM({
   env: process.env.NODE_ENV || '开发',
 })
 
-const executeJob = async () => {
-  const messages = []
-  let total = 0
-  let failedCount = 0
+class ValidateResultJob {
+  async executeJob() {
+    const messages = []
+    let total = 0
+    let failedCount = 0
 
-  try {
-    const apiDataValidator = new ApiDataValidator(require('./schema/lotto-api'))
-    const systemConfigs = await lottoApi.getSystemConfigs()
+    try {
+      const apiDataValidator = new ApiDataValidator(require('./schema/lotto-api'))
+      const systemConfigs = await lottoApi.getSystemConfigs()
 
-    for (const country of systemConfigs.countries) {
-      for (const level of country.levels) {
-        const resultFile = path.join(__dirname, 'result', 'result', moment().format('YYYYMMDD/hhmm'), `${country}-${level}.json`)
+      for (const country of systemConfigs.countries) {
+        for (const level of country.levels) {
+          const resultFile = path.join(__dirname, 'result', 'result', moment().format('YYYYMMDD/hhmm'), `${country}-${level}.json`)
 
-        try {
-          // get api data
-          const lotteryList = await lottoApi.getResults(country.code, level.code)
+          try {
+            // get api data
+            const lotteryList = await lottoApi.getResults(country.code, level.code)
 
-          // validate data
-          const result = apiDataValidator.validate('#/components/schemas/Result', lotteryList)
-          saveResult && apiDataValidator.saveResult(result, resultFile)
+            // validate data
+            const result = apiDataValidator.validate('#/components/schemas/Result', lotteryList)
+            saveResult && apiDataValidator.saveResult(result, resultFile)
 
-          total += result.total || 0
-          failedCount += result.failed || 0
+            total += result.total || 0
+            failedCount += result.failed || 0
 
-          const count = result.detail.length
-          let n = messages.length
-          let i = 0
-          while (n < 10 && i < count) {
-            messages.push(apiDataValidator.formatResult(country, level, result.detail[i]))
-            i++
-            n++
+            const count = result.detail.length
+            let n = messages.length
+            let i = 0
+            while (n < 10 && i < count) {
+              messages.push(apiDataValidator.formatResult(country, level, result.detail[i]))
+              i++
+              n++
+            }
+          } catch (err) {
+            saveResult && apiDataValidator.saveResult({ err }, resultFile)
+            im.error(`检查彩票开奖结果记录 ${country.name} ${level.name} 异常: ` + JSON.stringify(err))
           }
-        } catch (err) {
-          saveResult && apiDataValidator.saveResult({ err }, resultFile)
-          im.error(`检查彩票开奖结果记录 ${country.name} ${level.name} 异常: ` + JSON.stringify(err))
         }
       }
-    }
 
-    // 发送 slack 消息
-    im.error(`检查彩票开奖结果记录共 ${total} 条，失败 ${failedCount} 条，\n${messages.join('\n')}`)
-    console.log('DONE!')
-  } catch (err) {
-    console.error(err)
-    im.error('检查彩票开奖结果记录异常: ' + JSON.stringify(err))
+      // 发送 slack 消息
+      im.error(`检查彩票开奖结果记录共 ${total} 条，失败 ${failedCount} 条，\n${messages.join('\n')}`)
+      console.log('DONE!')
+    } catch (err) {
+      console.error(err)
+      im.error('检查彩票开奖结果记录异常: ' + JSON.stringify(err))
+    }
   }
 }
 
-executeJob() // start execute job!
+module.exports = new ValidateResultJob()
